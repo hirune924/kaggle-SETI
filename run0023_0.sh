b6c3836c57873c9d1a20859f2c37bfb2e9f5118e
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-06-04 18:17:20 +0900

    Update run0023_0.sh

--- run0023_0.sh
+++ run0023_0.sh
@@ -1,9 +1,9 @@
-python exp0023.py fold=0 batch_size=64 epoch=40 high=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold0
+python exp0023.py fold=0 batch_size=64 epoch=40 hight=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold0
 
-python exp0023.py fold=1 batch_size=64 epoch=40 high=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold1
+python exp0023.py fold=1 batch_size=64 epoch=40 hight=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold1
 
-python exp0023.py fold=2 batch_size=64 epoch=40 high=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold2
+python exp0023.py fold=2 batch_size=64 epoch=40 hight=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold2
 
-python exp0023.py fold=3 batch_size=64 epoch=40 high=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold3
+python exp0023.py fold=3 batch_size=64 epoch=40 hight=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold3
 
-python exp0023.py fold=4 batch_size=64 epoch=40 high=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold4
+python exp0023.py fold=4 batch_size=64 epoch=40 hight=512 width=512 model_name=efficientnet_b4 drop_rate=0.4 drop_path_rate=0.2 data_dir=/kqi/parent/22020972 output_dir=/kqi/output/fold4
