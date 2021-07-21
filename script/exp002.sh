99d4426bbf018c47d69aab4817b812b9d06f5bc7
hirune924 <nori.o920924@outlook.jp>
2021-07-21 17:49:58 +0900

    add script

---
+++ script/exp002.sh
@@ -0,0 +1,33 @@
+#[aaa,bcd]の2段階cosine annealing, tf-efficientnet-b4-ns
+#pip install -U timm
+python baseline_3.py fold=0 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold0_0
+
+python baseline_3.py fold=1 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold1_0
+
+python baseline_3.py fold=2 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold2_0
+
+python baseline_3.py fold=3 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold3_0
+
+python baseline_3.py fold=4 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold4_0
+
+
+
+python baseline_3.py fold=0 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+model_path=/kqi/output/fold0_0/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold0_1
+
+python baseline_3.py fold=1 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+model_path=/kqi/output/fold1_0/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold1_1
+
+python baseline_3.py fold=2 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+model_path=/kqi/output/fold2_0/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold2_1
+
+python baseline_3.py fold=3 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+model_path=/kqi/output/fold3_0/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold3_1
+
+python baseline_3.py fold=4 batch_size=48 epoch=20 height=512 width=512 model_name=tf_efficientnet_b4_ns drop_rate=0.4 drop_path_rate=0.2 \
+model_path=/kqi/output/fold4_0/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold4_1
