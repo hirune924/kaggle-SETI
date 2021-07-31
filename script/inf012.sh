ea53486132a409c9d628f71564e507f427026acb
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-07-31 22:34:16 +0900

    Update inf012.sh

--- script/inf012.sh
+++ script/inf012.sh
@@ -1,2 +1,2 @@
-pip install -U timm
+#pip install -U timm
 python inference_1.py batch_size=100 model_name=tf_efficientnet_b5_ns data_dir=/kqi/parent/22021621 output_dir=/kqi/output/ model_dir=/kqi/parent/22021769
