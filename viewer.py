03e79bda2525170e325e61812f3705f5b2a0a17f
hirune924 <nori.o920924@outlook.jp>
2021-08-04 14:44:38 +0900

    add viewer

---
+++ viewer.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+train_df = pd.read_csv('kqi/parent/22021621/train_labels.csv')
+test_df = pd.read_csv('pseudo802.csv')
+
+train_df['dir'] = 'kqi/parent/22021621/train'
+test_df['dir'] = 'kqi/parent/22021621/test'
+
+
+idx = 1
+img_id = train_df.loc[idx, 'id']
+img_path = os.path.join(train_df.loc[idx, 'dir'],"{}/{}.npy".format(img_id[0], img_id))
+image = np.load(file_path).astype(np.float32)
+image = np.vstack(image).transpose((1, 0))
+img_pl = Image.fromarray(image)#.resize((self.conf.height, self.conf.width), resample=Image.BICUBIC)
+st.image(img_pl)
+
+img_id = test_df.loc[idx, 'id']
+img_path = os.path.join(test_df.loc[idx, 'dir'],"{}/{}.npy".format(img_id[0], img_id))
+image = np.load(file_path).astype(np.float32)
+image = np.vstack(image).transpose((1, 0))
+img_pl = Image.fromarray(image)#.resize((self.conf.height, self.conf.width), resample=Image.BICUBIC)
+st.image(img_pl)
+
