9cdf905df456d449c7a42d100dc2e674493a34f0
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-08-05 15:20:55 +0900

    Update exp025.sh

--- script/exp025.sh
+++ script/exp025.sh
@@ -1,17 +1,17 @@
 #[aaa,bcd]の2段階cosine annealing, tf-efficientnet-b5-ns
 #pip install -U timm
 
-python baseline_1_semisup.py fold=0 batch_size=72 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+python baseline_1_semisup.py fold=0 batch_size=36 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.3 drop_path_rate=0.2 \
 model_path=/kqi/parent/22021767/fold0_1/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold0_0 trainer.accumulate_grad_batches=1
 
-python baseline_1_semisup.py fold=1 batch_size=72 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+python baseline_1_semisup.py fold=1 batch_size=36 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.3 drop_path_rate=0.2 \
 model_path=/kqi/parent/22021767/fold1_1/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold1_0 trainer.accumulate_grad_batches=1
 
-python baseline_1_semisup.py fold=2 batch_size=72 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+python baseline_1_semisup.py fold=2 batch_size=36 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.3 drop_path_rate=0.2 \
 model_path=/kqi/parent/22021767/fold2_1/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold2_0 trainer.accumulate_grad_batches=1
 
-python baseline_1_semisup.py fold=3 batch_size=72 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+python baseline_1_semisup.py fold=3 batch_size=36 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.3 drop_path_rate=0.2 \
 model_path=/kqi/parent/22021767/fold3_1/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold3_0 trainer.accumulate_grad_batches=1
 
-python baseline_1_semisup.py fold=4 batch_size=72 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+python baseline_1_semisup.py fold=4 batch_size=36 epoch=20 height=512 width=512 model_name=tf_efficientnet_b5_ns drop_rate=0.3 drop_path_rate=0.2 \
 model_path=/kqi/parent/22021767/fold4_1/ckpt/last.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold4_0 trainer.accumulate_grad_batches=1
