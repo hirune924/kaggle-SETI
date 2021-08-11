fcc94513353715e1183525ec9bbb42fca90cb7eb
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-08-11 16:09:03 +0900

    Create inf034.sh

---
+++ script/inf034.sh
@@ -0,0 +1,2 @@
+#pip install -U timm
+python inference_1_maxresize.py batch_size=100 model_name=tf_efficientnet_b5_ns data_dir=/kqi/parent/22021621 output_dir=/kqi/output/ model_dir=/kqi/parent/22021940 snap=1
