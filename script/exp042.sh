616556fc3ed7105984d0d4be4972c4cc8c4e90f5
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-08-12 11:52:10 +0900

    Create exp042.sh

---
+++ script/exp042.sh
@@ -0,0 +1,22 @@
+#[aaa,bcd]の2段階cosine annealing, tf-efficientnet-b5-ns
+#pip install -U timm
+
+python baseline_1_ns.py fold=0 batch_size=24 epoch=10 height=512 width=768 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+model_path=/kqi/parent/22021886/fold0_0/ckpt/fold0-epoch=19-val_score=0.90587.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold0_0 \
+pseudo=pseudo811.csv
+
+python baseline_1_ns.py fold=1 batch_size=24 epoch=10 height=512 width=768 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+model_path=/kqi/parent/22021886/fold1_0/ckpt/fold1-epoch=19-val_score=0.90199.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold1_0 \
+pseudo=pseudo811.csv
+
+python baseline_1_ns.py fold=2 batch_size=24 epoch=10 height=512 width=768 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+model_path=/kqi/parent/22021886/fold2_0/ckpt/fold2-epoch=19-val_score=0.90302.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold2_0 \
+pseudo=pseudo811.csv
+
+python baseline_1_ns.py fold=3 batch_size=24 epoch=10 height=512 width=768 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+model_path=/kqi/parent/22021886/fold3_0/ckpt/fold3-epoch=18-val_score=0.91562.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold3_0 \
+pseudo=pseudo811.csv
+
+python baseline_1_ns.py fold=4 batch_size=24 epoch=10 height=512 width=768 model_name=tf_efficientnet_b5_ns drop_rate=0.5 drop_path_rate=0.3 \
+model_path=/kqi/parent/22021886/fold4_0/ckpt/fold4-epoch=15-val_score=0.90265.ckpt data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold4_0 \
+pseudo=pseudo811.csv
