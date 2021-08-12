dd391f98b351de40fbe37689a26b2aa7ca54a3f0
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-08-12 09:24:06 +0900

    Update inference_1_maxresize.py

--- inference_1_maxresize.py
+++ inference_1_maxresize.py
@@ -223,6 +223,9 @@ def main():
     # get model path
     model_path = []
     for i in range(5):
+        if i == 3:
+            #model_path.append('/kqi/parent/22021886/fold3_0/ckpt/fold3-epoch=18-val_score=0.91562.ckpt')
+            continue
         for j in range(conf.snap):
             target_model = glob.glob(os.path.join(conf.model_dir, f'fold{i}_{j}/ckpt/*epoch*.ckpt'))
             scores = [float(os.path.splitext(os.path.basename(i))[0].split('=')[-1]) for i in target_model]
