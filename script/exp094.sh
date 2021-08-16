75d345db5926f2eac0c63903a8c05f4b827c38bc
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-08-16 19:16:07 +0900

    094

--- script/exp094.sh
+++ script/exp094.sh
@@ -1,10 +1,6 @@
 #[aaa,bcd]の2段階cosine annealing, tf-efficientnet-b5-ns
 pip install -U timm
 
-python baseline_1_ns_frac_wu.py fold=0 batch_size=20 epoch=20 height=512 width=512 model_name=tf_efficientnet_b7_ns drop_rate=0.5 drop_path_rate=0.3 \
-data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold0_0 \
-pseudo=pseudo813.csv
-
 python baseline_1_ns_frac_wu.py fold=1 batch_size=20 epoch=20 height=512 width=512 model_name=tf_efficientnet_b7_ns drop_rate=0.5 drop_path_rate=0.3 \
 data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold1_0 \
 pseudo=pseudo813.csv
@@ -19,4 +15,8 @@ pseudo=pseudo813.csv
 
 python baseline_1_ns_frac_wu.py fold=4 batch_size=20 epoch=20 height=512 width=512 model_name=tf_efficientnet_b7_ns drop_rate=0.5 drop_path_rate=0.3 \
 data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold4_0 \
+pseudo=pseudo813.csv
+
+python baseline_1_ns_frac_wu.py fold=0 batch_size=20 epoch=20 height=512 width=512 model_name=tf_efficientnet_b7_ns drop_rate=0.5 drop_path_rate=0.3 \
+data_dir=/kqi/parent/22021621 output_dir=/kqi/output/fold0_0 \
 pseudo=pseudo813.csv
