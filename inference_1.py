e50695ff002b7c32365f8b3a86297f5a417eea81
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-08-18 07:01:58 +0900

    skip 2,3

--- inference_1.py
+++ inference_1.py
@@ -221,8 +221,10 @@ def main():
     # get model path
     model_path = []
     for i in range(5):
-        if i == 0:
+        if i == 2:
             #model_path.append('/kqi/parent/22021886/fold3_0/ckpt/fold3-epoch=18-val_score=0.91562.ckpt')
+            continue
+        if i == 3:
             continue
         for j in range(conf.snap):
             target_model = glob.glob(os.path.join(conf.model_dir, f'fold{i}_{j}/ckpt/*epoch*.ckpt'))
